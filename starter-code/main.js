6e1cc2594e379af7e84cad60acad752670c6120b
nro5 <nro5@georgetown.edu>
2016-08-30 23:28:33 -0700

    Unit 11 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,31 +1,65 @@
 /* JS for Memory Game */
 
 // Card declarations
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardInPlay = [];
 
 
 
 // Game setup
 
+// TODO scoreboard and reset button
+// TODO random card placement
+// TODO more than 4 cards?
+
 function createBoard() {
     // Generate 4 cards
-    for (i = 0; i < 4; i++) {
-        console.log(i);
+    for (var i = 0; i < cards.length; i++) {
         var newCard = document.createElement('div');
         newCard.className = 'card';
-        newCard.innerHtml= 'hello';
+        newCard.setAttribute('data-card', cards[i]); // Identify the card
         document.getElementById('game-board').appendChild(newCard);
+        newCard.addEventListener('click', isTwoCards);
     }
 }
-
-createBoard();
-//var gameBoard = document.getElementById('game-board');
-
-
 
 
 
 // Game logic
+
+function isTwoCards() {
+    cardInPlay.push(this.getAttribute('data-card'));
+    if ((this.getAttribute('data-card') === 'king')) {
+        this.innerHTML = '<img src="PNG-cards-1.3/king_of_spades2.png" alt="King of Spades" />';
+    } else if ((this.getAttribute('data-card') === 'queen')) {
+        this.innerHTML = '<img src="PNG-cards-1.3/queen_of_spades2.png" alt="Queen of Spades" />';
+    }
+    console.log(cardInPlay.length);
+    if (cardInPlay.length === 2) {
+        isMatch(cardInPlay);
+        cardInPlay = [];
+    }
+};
+
+function isMatch(cardInPlay) {
+    if (cardInPlay[0] === cardInPlay[1]) {
+        alert("It's a match!");
+        console.log("Success!");
+    } else {
+        alert("Please try again.");
+        console.log("Not a match.");
+        resetBoard();
+    }
+
+}
+
+function resetBoard() {
+    document.getElementById('resetButton').removeAttribute('class', 'hidden');
+    for (var i = 0; i < cards.length; i++) {
+        document.getElementsByClassName('card')[i].innerHTML = '';
+    }
+}
+
+createBoard();
