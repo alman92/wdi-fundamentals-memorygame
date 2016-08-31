58319a5e8fab1185bcc4d5877ec3f7485de769e1
nro5 <nro5@georgetown.edu>
2016-08-30 21:37:23 -0700

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,10 +6,26 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-// Game logic
 
-if (cardTwo === cardFour) {
-    alert("Successful match!");
-} else {
-    alert("Sorry, try again.");
+
+// Game setup
+
+function createBoard() {
+    // Generate 4 cards
+    for (i = 0; i < 4; i++) {
+        console.log(i);
+        var newCard = document.createElement('div');
+        newCard.className = 'card';
+        newCard.innerHtml= 'hello';
+        document.getElementById('game-board').appendChild(newCard);
+    }
 }
+
+createBoard();
+//var gameBoard = document.getElementById('game-board');
+
+
+
+
+
+// Game logic
