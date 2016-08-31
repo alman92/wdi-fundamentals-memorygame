4fcd4f05f76ae2c0419740277bfc2d48c23bdaca
nro5 <nro5@georgetown.edu>
2016-08-30 17:24:55 -0700

    Unit 9 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,11 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+// Game logic
+
+if (cardTwo === cardFour) {
+    alert("Successful match!");
+} else {
+    alert("Sorry, try again.");
+}
